b61cd8bc3a5600a88f71ed4c909f959ce020da44
Miguel Reuter <miguel.reuter@laposte.net>
2019-04-06 23:06:56 +0200

    chore: improve display of sphere's shadow

--- src/Engine/Display/Debug3D.py
+++ src/Engine/Display/Debug3D.py
@@ -17,14 +17,21 @@ def draw_sphere(camera, center, radius):
 	draw.circle(camera.surface, DBG_COLOR_SPHERE, camera.world_to_pixel_coords(center), r_px)
 
 def draw_horizontal_ellipse(camera, pos, r):
-	# TODO : draft, messy
-	pts = [camera.world_to_pixel_coords(Vector3(pos) + (0, -r, 0)),
-	       camera.world_to_pixel_coords(Vector3(pos) + (0, r, 0))]
-	pts2 = [camera.world_to_pixel_coords(Vector3(pos) + (-r, 0, 0)),
-	        camera.world_to_pixel_coords(Vector3(pos) + (r, 0, 0))]
-	rect = Rect(pts[0], (abs(pts[1][0] - pts[0][0]), abs(pts2[1][1] - pts2[0][1])))
+	# corners of shadow (trapeze)
+	t_l = camera.world_to_pixel_coords(Vector3(pos) + (-r, -r, 0))
+	t_r = camera.world_to_pixel_coords(Vector3(pos) + (-r, r, 0))
+	b_l = camera.world_to_pixel_coords(Vector3(pos) + (r, -r, 0))
+	b_r = camera.world_to_pixel_coords(Vector3(pos) + (r, r, 0))
+
+	# approximate rect
+	r_pos = (int(0.5 * (t_l[0] + b_l[0])), t_l[1])
+	r_w = int(0.5 * (t_r[0] + b_r[0]) - r_pos[0])
+	r_h = b_r[1] - r_pos[1]
+	rect = Rect(r_pos, (r_w, r_h))
+
 	draw.ellipse(camera.surface, DBG_COLOR_SHADOW, rect)
-	draw.rect(camera.surface, (0, 255, 0), rect, 2)
+	draw.polygon(camera.surface, DBG_COLOR_SHADOW_TRAPEZE, [t_l, t_r, b_r, b_l], 1)
+
 
 def draw_polygon(camera, pts):
 	draw.polygon(camera.surface, DBG_COLOR_POLYGON, [(camera.world_to_pixel_coords(pt)) for pt in pts])
