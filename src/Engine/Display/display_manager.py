fccd7607a742327a54693f6e49e56fbbfd836fbd
Miguel Reuter <miguel.reuter@laposte.net>
2019-04-11 14:24:30 +0200

    refactor: move display methods/attributes (screen, resize, create window...) in display manager

--- src/Engine/Display/display_manager.py
+++ src/Engine/Display/display_manager.py
@@ -4,21 +4,86 @@ from Engine.Display.camera import Camera
 from settings import *
 
 import pygame as pg
+from math import log2, pow
 
 
 class DisplayManager:
 	# TODO : singleton
-	def __init__(self, game_engine, surface_size=NOMINAL_RESOLUTION, debug_surface_size=None):
+	def __init__(self, game_engine):
+		# will be set in self.create_window()
+		self.screen = None
+		self.unscaled_surface = None
+		self.scaled_surface = None
+
+		self.window_mode = WINDOW_MODE
+		self.window_resize_2n = WINDOW_RESIZE_2N
+		self.screen_scale_factor_2n = None
+		self._create_window()
+		
 		self.game_engine = game_engine
 		self.camera = Camera(self, CAMERA_POS, FOCUS_POINT, FOV_ANGLE)
-		self.surface = pg.Surface(surface_size)
-		self.debug_surface = pg.Surface(debug_surface_size if debug_surface_size is not None else surface_size)
-		self.debug_surface.set_colorkey((0, 0, 0))
 		
+		self.debug_surface = pg.Surface(self.screen.get_size())
+		self.debug_surface.set_colorkey((0, 0, 0))
+	
+	def _create_window(self):
+		w, h = NOMINAL_RESOLUTION
+		if self.window_mode == WindowMode.FIXED_SIZE:
+			if not self.window_resize_2n:
+				pass
+			else:
+				self.screen_scale_factor_2n = self._process_screen_factor_scale()
+				w = int(NOMINAL_RESOLUTION[0] * pow(2, self.screen_scale_factor_2n))
+				h = int(NOMINAL_RESOLUTION[1] * pow(2, self.screen_scale_factor_2n))
+		elif self.window_mode == WindowMode.RESIZABLE:
+			# TODO : to implement
+			print(self.window_mode, " mode not implemented")
+		elif self.window_mode == WindowMode.FULL_SCREEN:
+			# TODO : to implement
+			print(self.window_mode, " mode not implemented")
+		
+		self.screen = pg.display.set_mode((w, h))
+		self.scaled_surface = pg.Surface((w, h))
+		self.unscaled_surface = pg.Surface(NOMINAL_RESOLUTION)
+		
+		pg.display.set_caption(CAPTION_TITLE)
+		
+	def _resize_surface(self, surface):
+		if self.window_mode == WindowMode.FIXED_SIZE and self.window_resize_2n:
+			new_size = list(surface.get_size())
+			new_surface = pg.Surface(new_size)
+			new_surface.blit(surface, (0, 0))
+			for _ in range(self.screen_scale_factor_2n):
+				new_size = [new_size[i] * 2 for i in (0, 1)]
+				new_surface = pg.transform.scale2x(new_surface)
+			return new_surface
+		return surface
+	
+	def _process_screen_factor_scale(self):
+		f_w = int(log2(pg.display.Info().current_w / NOMINAL_RESOLUTION[0]))
+		f_h = int(log2(pg.display.Info().current_h / NOMINAL_RESOLUTION[1]))
+		return min(f_w, f_h)
+	
+	def _get_blit_position(self, size_1, size_2):
+		if size_1[0] >= size_2[0] and size_1[1] >= size_2[1]:
+			return tuple(int(size_1[i] / 2 - size_2[i] / 2) for i in (0, 1))
+		return (0, 0)
+	
 	def update(self, objects):
-		self.surface.fill((0, 0, 0))
+		self.screen.fill((50, 50, 0))
+		self.unscaled_surface.fill((0, 0, 50))
 		self.debug_surface.fill((0, 0, 0))
 		for obj in objects:
 			obj.draw(self)
+		
+		# resize
+		self.scaled_surface = self._resize_surface(self.unscaled_surface)
+		
+		self.screen.blit(self.scaled_surface, self._get_blit_position(self.screen.get_size(), self.scaled_surface.get_size()))
+		self.screen.blit(self.debug_surface, self._get_blit_position(self.screen.get_size(), self.debug_surface.get_size()))
+		# update screen
+		pg.display.flip()
+			
+	
 
 	
--- src/Engine/game_engine.py
+++ src/Engine/game_engine.py
@@ -10,19 +10,12 @@ from Game.court import Court
 from Game.character import Character
 
 import pygame as pg
-from math import log2, pow
+
 
 class GameEngine:
 	# TODO : singleton
 	def __init__(self):
-		self.screen = None  # will be set in self._create_window()
-		
-		self.window_mode = WindowMode.FIXED_SIZE
-		self.window_resize_2n = WINDOW_RESIZE_2N
-		self.screen_scale_factor_2n = None
-		self._create_window()
-		
-		self.display_manager = DisplayManager(self, debug_surface_size=self.screen.get_size())
+		self.display_manager = DisplayManager(self)
 		self.input_manager = InputManager(self)
 
 		self.running = True
@@ -34,37 +27,6 @@ class GameEngine:
 		self.char1 = Character((-2, -2, 0))
 		self.char2 = Character((2, 2, 0))
 		self.objects = [self.court, self.ball, self.char1, self.char2]
-		
-	def _create_window(self):
-		w, h = NOMINAL_RESOLUTION
-		if self.window_mode == WindowMode.FIXED_SIZE:
-			if not self.window_resize_2n:
-				pass
-			else:
-				self.screen_scale_factor_2n = self._process_screen_factor_scale()
-				w = int(NOMINAL_RESOLUTION[0] * pow(2, self.screen_scale_factor_2n))
-				h = int(NOMINAL_RESOLUTION[1] * pow(2, self.screen_scale_factor_2n))
-		elif self.window_mode == WindowMode.RESIZABLE:
-			# TODO : to implement
-			print(self.window_mode, " mode not implemented")
-		elif self.window_mode == WindowMode.FULL_SCREEN:
-			# TODO : to implement
-			print(self.window_mode, " mode not implemented")
-		
-		self.screen = pg.display.set_mode((w, h))
-		self.surface = pg.Surface((w, h))
-		pg.display.set_caption(CAPTION_TITLE)
-	
-	def _resize_surface(self, surface):
-		if self.window_mode == WindowMode.FIXED_SIZE and self.window_resize_2n:
-			new_size = list(surface.get_size())
-			new_surface = pg.Surface(new_size)
-			new_surface.blit(surface, (0, 0))
-			for _ in range(self.screen_scale_factor_2n):
-				new_size = [new_size[i] * 2 for i in (0, 1)]
-				new_surface = pg.transform.scale2x(new_surface)
-			return new_surface
-		return surface
 
 	def request_quit(self):
 		self.running = False
@@ -97,28 +59,18 @@ class GameEngine:
 			# PHYSICS
 			self.ball.update_physics(t2-t1)
 			
-			# DISPLAY
-			self.display_manager.update(self.objects)
-			
-			# TODO : move this in DisplayManager + display_manager.surface_wo_scale and display_manager.scaled_surface
-			new_surface = self._resize_surface(self.display_manager.surface)
-			self.surface.blit(new_surface, (0, 0))
-			
-			self.screen.blit(self.surface, (0, 0))
-			self.screen.blit(self.display_manager.debug_surface, (0, 0))
-
-			# update screen
-			pg.display.flip()
-
 			# KB EVENTS
 			self.input_manager.update()
-			
 			# TODO : take in account origin of action event (player index for ex.) ?
 			actions_events_queue = pg.event.get(ACTIONEVENT)
+			
 			# UPDATE ACTIONS
 			self.char1.update_actions(actions_events_queue, t2 - t1)
 			cam.update_actions(actions_events_queue, t2 - t1)
 			self.update_actions(actions_events_queue, t2 - t1)
+			
+			# DISPLAY
+			self.display_manager.update(self.objects)
 
 			# manage frame rate
 			t1 = t2
@@ -128,8 +80,5 @@ class GameEngine:
 
 		print("run with {} mean fps".format(int(1000 * frame_count / (t2 - t0))))
 
-	def _process_screen_factor_scale(self):
-		f_w = int(log2(pg.display.Info().current_w / NOMINAL_RESOLUTION[0]))
-		f_h = int(log2(pg.display.Info().current_h / NOMINAL_RESOLUTION[1]))
-		return min(f_w, f_h)
+
 		
