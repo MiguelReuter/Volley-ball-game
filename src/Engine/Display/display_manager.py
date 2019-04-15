7cfbbfdb7020ac13a7ab24eccbcc5dfbe0992f92
Miguel Reuter <miguel.reuter@laposte.net>
2019-04-15 11:17:08 +0200

    refactor: change _create_window method signature

--- src/Engine/Display/display_manager.py
+++ src/Engine/Display/display_manager.py
@@ -19,35 +19,43 @@ class DisplayManager:
 		self.window_mode = WINDOW_MODE
 		self.window_resize_2n = WINDOW_RESIZE_2N
 		self.screen_scale_factor_2n = None
-		self._create_window()
+		self._create_window(NOMINAL_RESOLUTION, self.window_mode, self.window_resize_2n)
 		
 		self.game_engine = game_engine
 		self.camera = Camera(self, CAMERA_POS, FOCUS_POINT, FOV_ANGLE)
 
-	def _create_window(self):
-		scaled_size = NOMINAL_RESOLUTION
-		screen_size = NOMINAL_RESOLUTION
+	def _create_window(self, nominal_resolution, window_mode=WindowMode.FIXED_SIZE, window_resize_2n=False):
+		"""
+		Create Game Window.
+		
+		:param tuple(int, int) nominal_resolution: nominal pixel resolution
+		:param WindowMode.Enum window_mode: rendered mode of window. Can be FIXED_SIZE, RESIZABLE or FULL_SCREEN
+		:param bool window_resize_2n: if True, window size is set to maximum according to your current display
+		with a factor 2^n. Nominal resolution is unchanged. Only in FIXED_SIZE and RESIZABLE modes
+		:return: None
+		"""
+		scaled_size = nominal_resolution
+		screen_size = nominal_resolution
 		fl = 0
 		
-		if self.window_mode == WindowMode.FIXED_SIZE or self.window_mode == WindowMode.RESIZABLE:
-			fl = pg.RESIZABLE if self.window_mode == WindowMode.RESIZABLE else 0
-			if not self.window_resize_2n:
+		if window_mode == WindowMode.FIXED_SIZE or window_mode == WindowMode.RESIZABLE:
+			fl = pg.RESIZABLE if window_mode == WindowMode.RESIZABLE else 0
+			if not window_resize_2n:
 				pass
 			else:
 				self.screen_scale_factor_2n = self._process_screen_factor_scale()
-				scaled_size = tuple(int(NOMINAL_RESOLUTION[i] * pow(2, self.screen_scale_factor_2n)) for i in (0, 1))
+				scaled_size = tuple(int(nominal_resolution[i] * pow(2, self.screen_scale_factor_2n)) for i in (0, 1))
 				screen_size = scaled_size
 				
-		elif self.window_mode == WindowMode.FULL_SCREEN:
+		elif window_mode == WindowMode.FULL_SCREEN:
 			fl = pg.FULLSCREEN
 			screen_size = pg.display.Info().current_w, pg.display.Info().current_h
-			print(screen_size)
 			
-			f_w, f_h = tuple(screen_size[i] / NOMINAL_RESOLUTION[i] for i in (0, 1))
+			f_w, f_h = tuple(screen_size[i] / nominal_resolution[i] for i in (0, 1))
 			f = min(f_w, f_h)
-			scaled_size = tuple(int(f * NOMINAL_RESOLUTION[i]) for i in (0, 1))
+			scaled_size = tuple(int(f * nominal_resolution[i]) for i in (0, 1))
 		
-		self.unscaled_surface = pg.Surface(NOMINAL_RESOLUTION)
+		self.unscaled_surface = pg.Surface(nominal_resolution)
 		self.scaled_surface = pg.Surface(scaled_size)
 		self.debug_surface = pg.Surface(scaled_size)
 		self.debug_surface.set_colorkey((0, 0, 0))
@@ -56,6 +64,14 @@ class DisplayManager:
 		pg.display.set_caption(CAPTION_TITLE)
 		
 	def resize_display(self):
+		"""
+		Resize display for each frame.
+		
+		Resize display according to attributes :
+			- :var WindowMode.Enum self.window_mode:
+			- :var bool self.window_resize_2n values:
+		:return: None
+		"""
 		surface = self.unscaled_surface
 		if self.window_mode == WindowMode.FIXED_SIZE and self.window_resize_2n:
 			new_size = list(surface.get_size())
@@ -86,25 +102,22 @@ class DisplayManager:
 			self.scaled_surface = self.unscaled_surface.copy()
 	
 	def _process_screen_factor_scale(self):
+		"""
+		Process a factor (2^n) to adapt window size.
+		
+		:return: None
+		"""
 		f_w = int(log2(pg.display.Info().current_w / NOMINAL_RESOLUTION[0]))
 		f_h = int(log2(pg.display.Info().current_h / NOMINAL_RESOLUTION[1]))
 		return min(f_w, f_h)
 	
-	@staticmethod
-	def _get_position_to_blit_centered_surfaces(size_1, size_2):
-		"""
-		Get position to draw a centered surface onto another.
-		
-		:param tuple(int, int) size_1: size of surface to draw on
-		:param tuple(int, int) size_2: size of surface to draw
-		:return: left-top corner position of surface to draw on
-		:rtype tuple(int, int):
-		"""
-		if size_1[0] >= size_2[0] and size_1[1] >= size_2[1]:
-			return tuple(int(size_1[i] / 2 - size_2[i] / 2) for i in (0, 1))
-		return (0, 0)
-	
 	def update(self, objects):
+		"""
+		Update and draw objects each frame.
+		
+		:param list() objects: list of objects to draw. Each object has to have a method : draw(self, display_manager).
+		:return: None
+		"""
 		self.screen.fill((50, 50, 0))
 		self.unscaled_surface.fill((0, 0, 50))
 		self.debug_surface.fill((0, 0, 0))
@@ -117,7 +130,17 @@ class DisplayManager:
 		self.screen.blit(self.debug_surface, self._get_position_to_blit_centered_surfaces(self.screen.get_size(), self.debug_surface.get_size()))
 		# update screen
 		pg.display.flip()
-			
 	
+	@staticmethod
+	def _get_position_to_blit_centered_surfaces(main_surface_size, surface_to_draw_size):
+		"""
+		Get position to draw a centered surface onto another.
 
-	
+		:param tuple(int, int) main_surface_size: size of main surface to draw on
+		:param tuple(int, int) surface_to_draw_size: size of surface to draw
+		:return: left-top corner position of surface to draw on
+		:rtype tuple(int, int):
+		"""
+		if main_surface_size[0] >= surface_to_draw_size[0] and main_surface_size[1] >= surface_to_draw_size[1]:
+			return tuple(int(main_surface_size[i] / 2 - surface_to_draw_size[i] / 2) for i in (0, 1))
+		return (0, 0)
