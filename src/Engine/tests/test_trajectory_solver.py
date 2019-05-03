03a5daa685406e14a6d34102ec902b6f6c7f8788
Miguel Reuter <miguel.reuter@laposte.net>
2019-05-03 17:05:33 +0200

    test: add tests for trajectory solver

--- src/Engine/tests/test_trajectory_solver.py
+++ src/Engine/tests/test_trajectory_solver.py
@@ -33,6 +33,13 @@ def test_find_initial_velocity():
 	assert vel.y < 0
 	assert vel.z > 0
 	
+	# throw along -y axis
+	target_pos = Vector3(5, 1, 0)
+	vel = find_initial_velocity(origin_pos, target_pos, wanted_height=1)
+	assert vel.x > 0
+	assert vel.y == 0
+	assert vel.z > 0
+	
 	# throw in 3D
 	target_pos = Vector3(-5, 3, 0.5)
 	vel = find_initial_velocity(origin_pos, target_pos, wanted_height=1)
@@ -47,4 +54,18 @@ def test_initial_velocity_and_target_position():
 	
 	initial_velocity = find_initial_velocity(origin_pos, target_position, wanted_height=1)
 	assert target_position == find_target_position(origin_pos, initial_velocity, wanted_z=0)
+	
+	
+def test_get_n_points_in_trajectory():
+	origin_pos = Vector3(0, 1, 0)
+	initial_velocity = Vector3(2, 0, 2)
+	n = 10
+	
+	target_pos = find_target_position(origin_pos, initial_velocity, 0)
+	
+	points = get_n_points_in_trajectory(n, origin_pos, initial_velocity, wanted_z=0)
+	
+	assert len(points) == n
+	assert points[0] == origin_pos
+	assert points[n-1] == target_pos
 	
