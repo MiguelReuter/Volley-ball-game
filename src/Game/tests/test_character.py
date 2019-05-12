09ebcd38ff102782af9fac2adc2becd610eb420c
Miguel Reuter <miguel.reuter@laposte.net>
2019-05-12 16:25:17 +0200

    test: add test for changing character collider

--- src/Game/tests/test_character.py
+++ src/Game/tests/test_character.py
@@ -41,3 +41,39 @@ def test_move(character):
 	assert character.collider.center == Vector3(4, 8, 0) + character.collider_relative_position
 
 
+def test_change_collider(character):
+	char_original_pos = Vector3(character.position)
+	char_original_collider_rel_pos = Vector3(character.collider_relative_position)
+	coll_original_size = Vector3(character.collider.size3)
+	coll_original_center = Vector3(character.collider.center)
+
+	# set default collider
+	character.set_default_collider()
+	assert character.collider.size3 == coll_original_size
+	assert character.collider.center == coll_original_center
+	assert character.position == char_original_pos
+	assert character.collider_relative_position == char_original_collider_rel_pos
+
+	# change and reset collider
+	direction = Vector3(0, 1, 0)
+	character.set_diving_collider(direction)
+	character.set_default_collider()
+	assert character.collider.size3 == coll_original_size
+	assert character.collider.center == coll_original_center
+	assert character.position == char_original_pos
+	assert character.collider_relative_position == char_original_collider_rel_pos
+
+	# move, change collider, move again and reset collider
+	direction = Vector3(0, 1, 0)
+
+	character.move(Vector3(1, 0, 0), 1000)
+	character.set_diving_collider(direction)
+	character.move(Vector3(0, 1, 0), 500)
+	character.set_default_collider()
+	assert character.collider.size3 == coll_original_size
+	assert character.collider.center == coll_original_center + character.max_velocity * Vector3(1, 0.5, 0)
+	assert character.position == char_original_pos + character.max_velocity * Vector3(1, 0.5, 0)
+	assert character.collider_relative_position == char_original_collider_rel_pos
+
+
+
