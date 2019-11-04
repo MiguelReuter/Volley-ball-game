f02b0a5070fd5ebbdbde6973505cd98c752492fa
Miguel Reuter <miguel.reuter@laposte.net>
2019-11-04 23:36:33 +0100

    test: add test for get_scaled_rect_from method

--- src/Engine/tests/test_scalable_sprite.py
+++ src/Engine/tests/test_scalable_sprite.py
@@ -1,6 +1,6 @@
 # encoding : UTF-8
 
-from Engine.Display.scalable_sprite import shift_rect_ip
+from Engine.Display.scalable_sprite import shift_rect_ip, get_scaled_rect_from
 from pygame import Rect
 
 
@@ -36,4 +36,28 @@ def test_shift_rect_ip():
 	assert rect.y == 2
 	assert rect.w == 0
 	assert rect.h == 10
+
+
+def test_get_scaled_rect():
+	rect = Rect(3, 7, 5, 10)
+
+	# 1
+	scaled_rect = get_scaled_rect_from(rect, 2)
+	assert scaled_rect.x == 6
+	assert scaled_rect.y == 14
+	assert scaled_rect.w == 10
+	assert scaled_rect.h == 20
+
+	# 2
+	scaled_rect = get_scaled_rect_from(rect, 1)
+	assert rect == scaled_rect
+
+	# 3
+	scaled_rect = get_scaled_rect_from(rect, 1/3)
+	assert scaled_rect.x == 3//3
+	assert scaled_rect.y == 7//3
+	assert scaled_rect.w == 5//3
+	assert scaled_rect.h == 10//3
+
+
 
